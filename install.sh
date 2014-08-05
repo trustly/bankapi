08f439a0e499ced67d7c9832e90d5bb8cbed9afc
Joel Jacobson <joel@trustly.com>
2014-08-05 11:09:47 +0200

    Add git checkout of pgcrypto_signatures to install script

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ sudo apt-get -y build-dep postgresql postgresql-9.3
 sudo apt-get -y install postgresql-9.3 postgresql-client-9.3 postgresql-contrib-9.3 postgresql-server-dev-9.3
 git clone https://github.com/johto/postgres.git
 cd postgres/contrib/pgcrypto
+git checkout pgcrypto_signatures
 touch pgcrypto--1.1--1.2.sql
 USE_PGXS=1 make
 sudo env USE_PGXS=1 make install
