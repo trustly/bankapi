1b06324ed056b37774f8477bd57cc9ca74af55ac
Per Lejontand <per.lejontand@trustly.com>
2014-08-07 09:34:51 +0200

    First draft of building .deb with the new data and files
    Including testdata and things added recently.

--- install.sql
+++ install.sql
@@ -1,8 +1,11 @@
 CREATE EXTENSION pgcrypto;
+
 \i TABLES/banks.sql
 \i TABLES/keys.sql
 \i TABLES/files.sql
 \i TABLES/messages.sql
+
+\i FK_CONSTRAINTS/banks_publickeyid_fkey.sql
 
 \i FUNCTIONS/encrypt_sign.sql
 \i FUNCTIONS/decrypt_verify.sql
---
+++ testdata/index.sql
@@ -0,0 +1,4 @@
+\i testdata/test-banks.sql
+\i testdata/test-publickeyrings.sql
+\i testdata/test-secretkeyring-TESTBANK001.sql
+\i testdata/test-secretkeyring-TESTBANK002.sql
