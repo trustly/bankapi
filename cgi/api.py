1eb855351bc8d9c3c4827af79318a0d1bc17407a
Per Lejontand <per.lejontand@trustly.com>
2014-08-08 11:49:56 +0200

    Remove the usage of JSON in communication.
    Read the POST data directly and use this as ciphertext. Respond by
    simply responding. Use HTTP response codes and reasons for communicating
    the status for the request. 200 OK will return a valid messagebody.

--- cgi/api.py
+++ cgi/api.py
@@ -10,97 +10,67 @@ import re
 pg_connect_str = "dbname=bankapi"
 pg_conn = None
 
-def respond(result=None, error=None):
+def respond(result=None, error=None, httperrorcode=None):
     global pg_conn
 
-    response = {
-            "version": "1.1"
-            }
-
     if result is not None:
-        response['result'] = result
+        print "Status: 200 OK"
+        print "Content-Type: text/plain"
+        print ""
+        print result
     else:
+        if httperrorcode is None:
+            httperrorcode = 500
         if error is None:
             error = 'Unknown error'
-        response['error'] = error
 
-    print "Content-Type: application/json"
-    print ""
-
-    json_str = json.dumps(response)
-    print json_str
+        print "Status: %s %s" % (httperrorcode, error)
 
     if pg_conn is not None:
         pg_conn.close()
 
     sys.exit(0)
 
-def verify_params(params, keys):
-    for key in keys:
-        if params.get(key) is None:
-            respond(error='Invalid %s' % (key))
+def error_log(s):
+    print "s" >> sys.stderr
 
 try:
     pg_conn = psycopg2.connect(pg_connect_str)
-except psycopg2.OperationalError as e:
-    respond(error='Failed to connect to database')
+except psycopg2.Error as e:
+    error_log("Failed to connect to database: error=%s, code=%, diag=%", e.pgerror, e.pgcode, e.diag)
+    respond(error='Failed to connect to database', httperrorcode=500)
 
 try:
-    indata = json.loads(sys.stdin.read())
-except ValueError as e:
-    respond(error='Bad input')
-except TypeError as e:
-    respond(error='Missing input')
+    indata = sys.stdin.read()
 except Exception as e:
-    respond(error='Failed to decode input data')
+    respond(error='Failed to decode input data', httperrorcode=400)
 
 if indata is not None:
-    try:
-        if str(indata['version']) != '1.1':
-            respond(error='Invalid version')
-    except KeyError as e:
-        respond(error='Missing version')
-
-    method = None
-    try:
-        method = indata['method']
-    except KeyError as e:
-        respond(error='Missing method')
-
-    params = None
-    try:
-        params = indata['params']
-    except KeyError as e:
-        respond(error='Missing params')
-
-    if type(params) is not dict:
-        respond(error='Invalid params')
-
     cur = pg_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-    if method == 'receive_message':
-        sqldata = None
-        try:
-            verify_params(params, ['ciphertext'])
-            cur.execute('SELECT deliveryreceipt FROM receive_message(%s)',
-                    [params['ciphertext']])
+    sqldata = None
+    try:
+        cur.execute('SELECT DeliveryReceipt FROM receive_message(%s)',
+                [indata])
 
-            sqldata = cur.fetchone()
-            pg_conn.commit()
-            respond(result=dict(sqldata))
-        except KeyError as e:
-            pg_conn.rollback()
-        except psycopg2.InternalError as e:
-            pg_conn.rollback()
-            mg = re.match('ERROR:\s+(ERROR_\S+)', e.pgerror)
-            if mg is not None:
-                respond(error=mg.group(1))
-            else:
-                respond(error='Receieve processing failed')
-        except psycopg2.ProgrammingError as e:
-            pg_conn.rollback()
-            respond(error='Recieve processing failed')
-    else:
-        respond(error='Invalid method')
+        sqldata = cur.fetchone()
+        pg_conn.commit()
+        respond(result=sqldata['deliveryreceipt'])
+    except psycopg2.InternalError as e:
+        pg_conn.rollback()
+        error_log("Call to Receive_Message failed: error=%s, code=%, diag=%", e.pgerror, e.pgcode, e.diag)
+
+        mg = re.match('ERROR:\s+(ERROR_\S+)', e.pgerror)
+        if mg is not None:
+            respond(error=mg.group(1), httperrorcode=500)
+        else:
+            respond(error='Receieve processing failed', httperrorcode=500)
+
+    except psycopg2.ProgrammingError as e:
+        error_log("Call to Receive_Message failed with ProgrammingError: error=%s, code=%, diag=%", e.pgerror, e.pgcode, e.diag)
+        pg_conn.rollback()
+        respond(error='Recieve processing failed', httperrorcode=500)
+else:
+    respond(error='Invalid method', httperrorcode=404)
 
 sys.exit(0)
