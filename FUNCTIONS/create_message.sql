56251bf248081ae68b11f726d8cd048538fa82eb
Joel Jacobson <joel@trustly.com>
2014-08-08 11:18:23 +0200

    Add MessageType input param

--- FUNCTIONS/create_message.sql
+++ FUNCTIONS/create_message.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION Create_Message(OUT MessageID text, _Plaintext text, _FromBankID text, _ToBankID text) RETURNS TEXT AS $BODY$
+CREATE OR REPLACE FUNCTION Create_Message(OUT MessageID text, _Plaintext text, _MessageType text, _FromBankID text, _ToBankID text) RETURNS TEXT AS $BODY$
 DECLARE
 _FileID text;
 _SignatureKeyID text;
@@ -12,7 +12,7 @@ IF NOT EXISTS (SELECT 1 FROM Files WHERE FileID = _FileID) THEN
     INSERT INTO Files (FileID, Plaintext) VALUES (_FileID, _Plaintext) RETURNING TRUE INTO STRICT _OK;
 END IF;
 
-SELECT Messages.MessageID INTO MessageID FROM Messages WHERE FileID = _FileID AND FromBankID = _FromBankID AND ToBankID = _ToBankID;
+SELECT Messages.MessageID INTO MessageID FROM Messages WHERE FileID = _FileID AND MessageType = _MessageType AND FromBankID = _FromBankID AND ToBankID = _ToBankID;
 IF FOUND THEN
     RETURN;
 END IF;
@@ -23,8 +23,8 @@ SELECT SubKeyID  INTO STRICT _EncryptionKeyID FROM Keys WHERE BankID = _ToBankID
 _Cipherdata := Encrypt_Sign(_Plaintext, _EncryptionKeyID, _SignatureKeyID);
 MessageID := encode(digest(_Cipherdata,'sha512'),'hex');
 
-INSERT INTO Messages (MessageID,  FileID,  FromBankID,  ToBankID,  Cipherdata)
-VALUES               (MessageID, _FileID, _FromBankID, _ToBankID, _Cipherdata)
+INSERT INTO Messages (MessageID,  MessageType,  FileID,  FromBankID,  ToBankID,  Cipherdata)
+VALUES               (MessageID, _MessageType, _FileID, _FromBankID, _ToBankID, _Cipherdata)
 RETURNING TRUE INTO STRICT _OK;
 
 RETURN;
