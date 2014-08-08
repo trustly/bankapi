12fb857e4b4282b3df195b45e114abdb9dab1d73
Joel Jacobson <joel@trustly.com>
2014-08-08 11:18:34 +0200

    Allow input param MessageID to Get_Message() to be partial and throw error if multiple rows or no rows could be found

--- FUNCTIONS/get_message.sql
+++ FUNCTIONS/get_message.sql
@@ -1,3 +1,8 @@
 CREATE OR REPLACE FUNCTION Get_Message(OUT Ciphertext text, _MessageID text) RETURNS TEXT AS $BODY$
-SELECT armor(Cipherdata) FROM Messages WHERE MessageID = $1
-$BODY$ LANGUAGE sql VOLATILE;
+DECLARE
+BEGIN
+EXECUTE 'SELECT armor(Cipherdata) FROM Messages WHERE MessageID LIKE $1' INTO STRICT Ciphertext USING (_MessageID || '%');
+RETURN;
+END;
+$BODY$ LANGUAGE plpgsql VOLATILE;
+
